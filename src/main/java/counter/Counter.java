a82ea7dade9b51d249fbc22cca0a86945583953c
amir-original <amirrahmani7017@gmail.com>
2023-02-16 20:41:36 +0330

    move global data into new class and create getter and setter for it and replace it by old data

--- src/main/java/counter/Counter.java
+++ src/main/java/counter/Counter.java
@@ -1,2 +1,13 @@
-package counter;public class Counter {
+package counter;
+
+public class Counter {
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 }
--- src/main/java/counter/Main.java
+++ src/main/java/counter/Main.java
@@ -4,15 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
-            incrementCounter();
-            System.out.println(counter);
+            Counter counter = new Counter();
+            incrementCounter(counter);
+            System.out.println(counter.getCounter());
         }
 
     }
 
-   static int counter = 0;
-   static void incrementCounter(){
-        counter++;
+
+   static void incrementCounter(Counter counter){
+        counter.setCounter(counter.getCounter() + 1);
     }
 
 }
