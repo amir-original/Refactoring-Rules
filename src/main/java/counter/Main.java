2de5ecc39bcb2772ed2708d9974bb4d928837c1c
amir-original <amirrahmani7017@gmail.com>
2023-02-16 20:49:16 +0330

    inline incrementCounter method

--- src/main/java/counter/Main.java
+++ src/main/java/counter/Main.java
@@ -5,15 +5,11 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
             Counter counter = new Counter();
-            incrementCounter(counter);
+            counter.increment();
             System.out.println(counter.getCounter());
         }
 
     }
 
-
-   static void incrementCounter(Counter counter){
-        counter.increment();
-    }
 
 }
